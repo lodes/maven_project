f54bd5d9f59cebae280e058137dd09a420500eb4
lodes <gaocaisheng2009@163.com>
2018-10-28 18:08:12 +0800

    新增加一条输出语句后提交

--- src/main/com.xinyonghua.controller/HelloWorld.java
+++ src/main/com.xinyonghua.controller/HelloWorld.java
@@ -3,5 +3,7 @@ package main.com.xinyonghua.controller;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("欢迎来到美丽的java");
+        System.out.println("从git拉取后修改后提交");
     }
+
 }
