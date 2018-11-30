68fb182e126aa85a93c8168fd5c9fb2eeeb1c828
lodes <gaocaisheng2009@163.com>
2018-11-30 22:03:40 +0800

    冲突提交本地仓库

--- src/main/com.xinyonghua.controller/HelloWorld.java
+++ src/main/com.xinyonghua.controller/HelloWorld.java
@@ -2,7 +2,7 @@ package main.com.xinyonghua.controller;
 
 public class HelloWorld {
     public static void main(String[] args) {
-    	System.out.println("eclipse 输出3");
+        System.out.println("idea 输出4");
         System.out.println("欢迎来到美丽的java");
         System.out.println("从git拉取后修改后提交");
          System.out.println("在GitHub上直接修改后");
@@ -10,9 +10,8 @@ public class HelloWorld {
         System.out.println("修改信差关键的分支");
         System.out.println("idea新加输出2");
         System.out.println("在GitHub上修改分支");
-        System.out.println("eclipse新加输出1");
         System.out.println("idea新加输出1");
-        System.out.println("eclipse 输出2");
+        System.out.println("idea 输出3");
         System.out.println("elcipse 输出4");
     }
 
