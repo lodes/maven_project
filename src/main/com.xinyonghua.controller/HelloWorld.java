4f843477836136db87ad1bd0d28bc5696d47bef3
lodes <gaocaisheng2009@163.com>
2018-11-30 21:41:37 +0800

    idea 第二次提交

--- src/main/com.xinyonghua.controller/HelloWorld.java
+++ src/main/com.xinyonghua.controller/HelloWorld.java
@@ -2,6 +2,7 @@ package main.com.xinyonghua.controller;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("idea 输出4");
         System.out.println("欢迎来到美丽的java");
         System.out.println("从git拉取后修改后提交");
          System.out.println("在GitHub上直接修改后");
@@ -10,6 +11,7 @@ public class HelloWorld {
         System.out.println("idea新加输出2");
         System.out.println("在GitHub上修改分支");
         System.out.println("idea新加输出1");
+        System.out.println("idea 输出3");
     }
 
 }
