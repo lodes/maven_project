6f714feb2f7b5efacd21d6b8809aecd86b94ca35
lodes <gaocaisheng2009@163.com>
2018-11-30 21:39:58 +0800

    eclipse 第二次提交

--- src/main/com.xinyonghua.controller/HelloWorld.java
+++ src/main/com.xinyonghua.controller/HelloWorld.java
@@ -2,6 +2,7 @@ package main.com.xinyonghua.controller;
 
 public class HelloWorld {
     public static void main(String[] args) {
+    	System.out.println("eclipse 输出3");
         System.out.println("欢迎来到美丽的java");
         System.out.println("从git拉取后修改后提交");
          System.out.println("在GitHub上直接修改后");
@@ -11,6 +12,7 @@ public class HelloWorld {
         System.out.println("在GitHub上修改分支");
         System.out.println("eclipse新加输出1");
         System.out.println("idea新加输出1");
+        System.out.println("eclipse 输出2");
     }
 
 }
